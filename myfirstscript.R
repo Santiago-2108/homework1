c5ed0a638dede291520ef5516844619b180d0022
Santiago-2108 <65274206+Santiago-2108@users.noreply.github.com>
2020-05-22 15:34:47 +0100

    Add files via upload

---
+++ myfirstscript.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(dplyr)
+library(dslabs)
+library(ggplot2)
+data(gapminder)
+world_income <- read.csv(file="income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
+print(world_income)
+
