ccddc8c745b20d2e9a1abaaf3ebb2f48770c4b54
Santiago-2108 <65274206+Santiago-2108@users.noreply.github.com>
2020-05-22 15:52:25 +0100

    updated myfirstscriptR
    filter four countries of interest.

--- myfirstscript.R
+++ myfirstscript.R
@@ -5,4 +5,6 @@ library(ggplot2)
 data(gapminder)
 world_income <- read.csv(file="income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
 print(world_income)
+James here.
+I will be filtering this dataset for four countries of interest: United States, Nigeria, Ghana, and Rwanda
 
